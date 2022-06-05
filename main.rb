045bdf652c38458df84ce14c36933d16322ad4b6
bl1ndy <ayoblindstone@gmail.com>
2022-06-05 19:24:43 +0300

    Add entry point

---
+++ main.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'cli/interface'
+require_relative 'models/deck'
+require_relative 'models/card'
+require_relative 'models/player'
+
+Interface.new.call
